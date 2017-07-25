a7387ca17bdb0f842785f5baa224453e41ff0657
Ritam Mehta <mehtaritam@gmail.com>
2017-07-25 14:50:03 -0400

    updated glossary API

--- app.py
+++ app.py
@@ -23,22 +23,8 @@ def all():
 @app.route('/glossary')
 def glossary():
     data = retrieve_data('1NCp-R2gipFeuG5l19iBeJxLf8N5oQUY9Qj3roWA8SLE', 'A1:B')
-    data = json.loads(json.dumps(data)) #this creates an array of dicts
-    if 'term' in request.args:
-        reqTerm = request.args['term'].lower()
-        foundWord = 0 #flag to see if the word has been found in the glossary
-        #checks to see if a request for a term was included
-        for x in data:
-            #iterate through the entire glossary and find the term you need
-            if (x['Term'] == reqTerm):
-                resp = Response(response=x['Definition'], status=200, mimetype="text/plain")
-                foundWord = 1 #set flag
-                break #if the word was found, exit the loop
-        if foundWord == 0:
-            #if the word wasn't found, return an error message
-            resp = Response(response='Could not find term in glossary', status=200, mimetype="text/plain")
-    else:
-        resp = Response(response='No term for you!', status=200, mimetype="text/plain") #otherwise prints an error message
+    data = json.dumps(data)
+    resp = Response(response=data, status=200, mimetype="application/json")
     return resp
 
 @app.route('/ican')
